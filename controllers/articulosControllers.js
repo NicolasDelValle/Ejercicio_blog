f33c6c619e788fbbb1af1be159b51e915b554d10
Adrian Del Valle <adrianchelo7@gmail.com>
2022-02-12 15:32:31 -0300

    Vista del home

--- controllers/articulosControllers.js
+++ controllers/articulosControllers.js
@@ -1,24 +1,22 @@
 const { Articulo } = require("../models");
 
 async function showAllArticles(req, res) {
-  const articles = await Articulo.findAll();
-  console.log(articles);
-  res.render("home", { articles });
+  const Articulos = await Articulo.findAll();
+  console.log("[USUARIO]Entro al home y existen : ( " + Articulos.length + " ) usuarios");
+  res.render("home", { Articulos });
 }
 
 async function showArticle(req, res) {
-  const {id} = req.params;
+  const { id } = req.params;
 
   const article = await Articulo.findByPk(id);
 
-  console.log("Entró a un artículo");
+  console.log("[USUARIO]Entró a un artículo");
 
-  res.render('articulo', {article})
+  res.render("articulo", { article });
 }
-
 
 module.exports = {
   showAllArticles,
   showArticle,
 };
-  
--- routes/articulosRoutes.js
+++ routes/articulosRoutes.js
@@ -3,7 +3,6 @@ const route = express.Router();
 const { showAllArticles, showArticle } = require("../controllers/articulosControllers");
 
 route.get("/", showAllArticles);
-
 route.get("/articulo/:id", showArticle);
 
 module.exports = route;
